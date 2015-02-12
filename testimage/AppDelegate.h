446f0db701ce1b4a8267368354e63d7f125b146b
yao.xue <yao.xue@qunar.com>
2015-02-12 18:32:05 +0800

    Initial Commit

---
+++ testimage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testimage
+//
+//  Created by xueyao on 15/2/12.
+//  Copyright (c) 2015年 xueyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testimage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testimage
+//
+//  Created by xueyao on 15/2/12.
+//  Copyright (c) 2015年 xueyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
